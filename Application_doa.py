6c40b7f025f3a67baa704919fe46736f47a27844
Dhruvin Vasani <55601851+dhruvin06@users.noreply.github.com>
2024-03-06 23:23:50 -0500

    Code

--- Application_doa.py
+++ Application_doa.py
@@ -2,7 +2,19 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user='root', password='',
                               host='127.0.0.1',
-                              database='employees')
+                              database='bank')
+
+cursor = cnx.cursor()
+
+
+query = ("SELECT Name , Balance From Users")
+
+
+cursor.execute(query)
+
+for (Name , Balance) in cursor:
+    print(Name , Balance)
 
 
 cnx.close()
+cnx.close
