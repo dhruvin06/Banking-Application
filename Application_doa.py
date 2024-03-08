f3a26a7d85783838a3df7dfdf68e46f47c300280
Dhruvin Vasani <dhruvin6102@gmail.com>
2024-03-07 20:54:26 -0500

    CODE

--- Application_doa.py
+++ Application_doa.py
@@ -1,20 +1,37 @@
-import mysql.connector
+from sql_connection import get_sql_connection
 
-cnx = mysql.connector.connect(user='root', password='',
-                              host='127.0.0.1',
-                              database='bank')
-
-cursor = cnx.cursor()
+def get_user(connection):
+    cursor = connection.cursor()
 
 
-query = ("SELECT Name , Balance From Users")
+    query = ("SELECT account_number , Name , Balance From Users")
 
 
-cursor.execute(query)
+    cursor.execute(query)
 
-for (Name , Balance) in cursor:
-    print(Name , Balance)
+    for (account_number , Name , Balance) in cursor:
+        print(account_number ,Name , Balance)
 
+    cursor.close()
+    
+    
+def add_users(connection):
+    cursor = connection.cursor()
 
-cnx.close()
-cnx.close
+    add_user = ("INSERT INTO Users"
+                "(Name , Balance)"
+                "VALUES (%s, %s)")
+            
+        
+    data_user = ("Dhruvin", "4444")    
+    cursor.execute(add_user , data_user)
+
+    connection.commit()
+
+    cursor.close()
+    
+
+if __name__ == '__main__':
+    connection =  get_sql_connection()
+    get_user(connection) 
+    add_users(connection)
