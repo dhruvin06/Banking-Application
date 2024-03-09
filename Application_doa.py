e876017715345ab468e2002df85724bac0f55f81
Dhruvin Vasani <dhruvin6102@gmail.com>
2024-03-08 19:45:01 -0500

    Code

--- Application_doa.py
+++ Application_doa.py
@@ -1,16 +1,16 @@
 from sql_connection import get_sql_connection
-
+import time 
 def get_user(connection):
     cursor = connection.cursor()
 
 
-    query = ("SELECT account_number , Name , Balance From Users")
+    query = ("SELECT a.account_number , a.Name , a.Balance , a.branch_id ,b.branch_name From Users as a join branch as b on a.branch_id = b.branch_id")
 
 
     cursor.execute(query)
 
-    for (account_number , Name , Balance) in cursor:
-        print(account_number ,Name , Balance)
+    for (account_number , Name , Balance , branch_id , branch_name ) in cursor:
+        print(account_number ,Name , Balance , branch_id , branch_name)
 
     cursor.close()
     
@@ -19,19 +19,41 @@ def add_users(connection):
     cursor = connection.cursor()
 
     add_user = ("INSERT INTO Users"
-                "(Name , Balance)"
-                "VALUES (%s, %s)")
+                "(Name , Balance,branch_id)"
+                "VALUES (%s, %s , %s)")
             
         
-    data_user = ("Dhruvin", "4444")    
+    data_user = ("Dhruvin1", "4444" ,1)    
     cursor.execute(add_user , data_user)
 
     connection.commit()
 
     cursor.close()
     
+def delete_user(connection):
+    cursor = connection.cursor()
+
+    delete_user = "Delete from users where Name = %s"
+    
+    data_user = ("Dhruvin" , )
+
+    cursor.execute(delete_user,data_user)
+
+    connection.commit()
+
+    cursor.close()
+
+
+
+
+
+
 
 if __name__ == '__main__':
     connection =  get_sql_connection()
-    get_user(connection) 
-    add_users(connection)
+    get_user(connection)
+    add_users(connection) 
+    delete_user(connection)
+    
+    
+    
